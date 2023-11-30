6b171e61b3c1377d8798eadbdc5b1d5cf7349083
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-30 17:04:51 +0530

    New addition done

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         DemoLinkedList D1 = new DemoLinkedList();
-        D1.addAtEnd(56);
         D1.addAtEnd(30);
+        D1.addAtEnd(56);
         D1.addAtEnd(70);
         D1.display();
     }
